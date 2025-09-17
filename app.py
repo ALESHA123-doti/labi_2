8578d9160846615f4f700456a4ea44b06e5d4443
Алёшкина Варя <hihikok678@mail.ru>
2025-09-17 17:13:27 +0700

    Стили

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def not_found(err):
 @app.route("/")
 @app.route("/web")
 def web():
-    return """<!doctype html> \
-        <html> \
-            <body> \
+    return """<!doctype html> 
+        <html> 
+            <body> 
                  <h1>web-сервер на flask</h1> 
                  <a href="/author">author</a>
-            </body> \
+            </body> 
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type':'text/plan; charset=utf-8'
@@ -24,8 +24,8 @@ def author():
     name = "Алёшкина Варвара Максимовна"
     group = "ФБИ-34"
     faculty = "ФБ"
-
-    return """<!doctype html>
+    return """
+<!doctype html>
 <html>
     <body>
         <p>Студент: """ + name + """</p>
@@ -37,13 +37,17 @@ def author():
 
 @app.route('/image')
 def image():
+    css_url = url_for('static', filename='lab1.css')
     path = url_for("static", filename="silent.jpg")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="''' + css_url + '''">
+    </head>
     <body>
         <h1>Тишина</h1>
-        <img src="'''+ path +'''">
+        <img src="''' + path + '''">
     </body>
 </html>'''
 
