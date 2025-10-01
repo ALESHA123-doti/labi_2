1c662a58704fed599cc27e2323806a249f872202
Алёшкина Варя <hihikok678@mail.ru>
2025-10-01 17:14:24 +0700

    Выражения правки

--- app.py
+++ app.py
@@ -380,3 +380,18 @@ def filters():
     phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
     return render_template('filter.html', phrase=phrase)
 
+# --- МАТЕМАТИЧЕСКИЙ КАЛЬКУЛЯТОР
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    return render_template('calc.html', a=a, b=b)
+
+# Редирект: /lab2/calc/ -> /lab2/calc/1/1
+@app.route('/lab2/calc/')
+def calc_default():
+    return redirect('/lab2/calc/1/1')
+
+# Редирект: /lab2/calc/<a> -> /lab2/calc/a/1
+@app.route('/lab2/calc/<int:a>')
+def calc_with_one_param(a):
+    return redirect(f'/lab2/calc/{a}/1')
