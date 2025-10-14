988878618d2ab802867a985a77daadbaf53b9e61
Алёшкина Варя <hihikok678@mail.ru>
2025-10-15 02:36:19 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, make_response, redirect
+
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
@@ -36,3 +37,35 @@ def form1():
         errors['age'] = 'Заполните поле!'
 
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+    
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    milk = request.args.get('milk')
+    sugar = request.args.get('sugar')
+
+    # Определяем базовую стоимость напитка
+    drink_prices = {
+        'cofee': 120,
+        'black-tea': 80,
+        'green-tea': 70
+    }
+    price += drink_prices.get(drink, 0)
+
+    # Добавки
+    if milk == 'on':
+        price += 30
+    if sugar == 'on':
+        price += 10
+
+    return render_template('lab3/pay.html', price=price, drink=drink, milk=milk, sugar=sugar)
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', '0')
+    return render_template('lab3/success.html', price=price)
