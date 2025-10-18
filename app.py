fd2a18cc4d67d41d21c9769fbc352fdbc392e4ad
Алёшкина Варя <hihikok678@mail.ru>
2025-10-18 10:39:09 +0700

    Деление

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from werkzeug.exceptions import HTTPException
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.errorhandler(404)
 def not_found(err):
