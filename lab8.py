d9bf54f4a9aad2b5e732eee43afffda32a876ab5
Алёшкина Варя <hihikok678@mail.ru>
2025-12-25 01:16:34 +0700

    Самостоятельные задания

--- lab8.py
+++ lab8.py
@@ -1,17 +1,19 @@
-from flask import Blueprint, render_template, request, redirect, session
+# lab8.py
+from flask import Blueprint, render_template, request, redirect, flash
 from db import db
 from db.models import users, articles
-lab8 = Blueprint('lab8', __name__)
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
-from werkzeug.security import check_password_hash
 
+lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def lab():
     login = current_user.login if current_user.is_authenticated else None
     return render_template('lab8/lab8.html', login=login)
 
+
+# РЕГИСТРАЦИЯ
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -32,8 +34,12 @@ def register():
     db.session.add(new_user)
     db.session.commit()
 
+    # Задание 2: автоматический вход после регистрации
+    login_user(new_user, remember=False)
     return redirect('/lab8/')
 
+
+# ВХОД
 @lab8.route('/lab8/login/', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -41,29 +47,87 @@ def login():
 
     login = request.form.get('login')
     password = request.form.get('password')
-
-    # Проверка на пустые поля
+    remember = request.form.get('remember') == 'on'#галочка
     if not login or not password:
         return render_template('lab8/login.html', error="Логин и пароль не могут быть пустыми")
-
-    # Ищем пользователя
+    
     user = users.query.filter_by(login=login).first()
 
-    # Проверяем пароль
     if user and check_password_hash(user.password, password):
-        login_user(user, remember=False)  # авторизуем
+        login_user(user, remember=remember)  #remember=True если галочка
         return redirect('/lab8/')
-
-    # Если не найден или пароль не совпал
+    
     return render_template('lab8/login.html', error="Неверный логин или пароль")
 
-@lab8.route('/lab8/articles/')
-@login_required
-def article_list():
-    return "список статей"
 
+#ЛОГАУТ
 @lab8.route('/lab8/logout/')
 @login_required
 def logout():
     logout_user()
     return redirect('/lab8/')
+
+
+#СПИСОК СТАТЕЙ (только для авторизованных)
+@lab8.route('/lab8/list/')
+@login_required
+def article_list():
+    user_articles = articles.query.filter_by(login_id=current_user.id).all()
+    return render_template('lab8/articles.html', articles=user_articles)
+
+
+# СОЗДАНИЕ СТАТЬИ. Задание 4
+@lab8.route('/lab8/create/', methods=['GET', 'POST'])
+@login_required
+def create_article():
+    if request.method == 'GET':
+        return render_template('lab8/create_article.html')
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    if not title or not article_text:
+        return render_template('lab8/create_article.html', error="Заголовок и текст обязательны")
+
+    new_article = articles(
+        login_id=current_user.id,
+        title=title,
+        article_text=article_text,
+        is_favorite=False,
+        is_public=True,
+        likes=0
+    )
+    db.session.add(new_article)
+    db.session.commit()
+    return redirect('/lab8/articles/')
+
+
+# РЕДАКТИРОВАНИЕ СТАТЬИ. Задание 5
+@lab8.route('/lab8/edit/<int:article_id>/', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first_or_404()
+
+    if request.method == 'GET':
+        return render_template('lab8/edit_article.html', article=article)
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    if not title or not article_text:
+        return render_template('lab8/edit_article.html', article=article, error="Заголовок и текст обязательны")
+
+    article.title = title
+    article.article_text = article_text
+    db.session.commit()
+    return redirect('/lab8/articles/')
+
+
+# УДАЛЕНИЕ СТАТЬИ. Задание 6
+@lab8.route('/lab8/delete/<int:article_id>/')
+@login_required
+def delete_article(article_id):
+    article = articles.query.filter_by(id=article_id, login_id=current_user.id).first_or_404()
+    db.session.delete(article)
+    db.session.commit()
+    return redirect('/lab8/articles/')
