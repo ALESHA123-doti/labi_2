e9cfc4a527badc7b64b0d99a87f77542219a2a71
Алёшкина Варя <hihikok678@mail.ru>
2025-12-25 00:42:10 +0700

    Логаут

--- lab8.py
+++ lab8.py
@@ -3,8 +3,9 @@ from db import db
 from db.models import users, articles
 lab8 = Blueprint('lab8', __name__)
 from werkzeug.security import generate_password_hash
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash
+
 
 @lab8.route('/lab8/')
 def lab():
@@ -60,3 +61,9 @@ def login():
 @login_required
 def article_list():
     return "список статей"
+
+@lab8.route('/lab8/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/lab8/')
